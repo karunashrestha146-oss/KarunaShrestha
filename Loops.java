7493b77d5136f337e10b93ac352ed7deb2e8fd2f
karunashrestha146-oss <karunashrestha146@gmail.com>
2026-01-12 23:05:08 +0530

    add all

---
+++ Loops.java
@@ -0,0 +1,19 @@
+package KarunaShrestha;
+
+public class Loops {
+    public static void main(String[] args) {
+        // (int i = 1; i < 10; i = i + 1) {
+        // System.out.println(i);
+
+        // while (i < 11) {
+        // System.out.println(i);
+        // i++;
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+
+        } while (i < 11);
+
+    }
+}
---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+package KarunaShrestha;
+
+import java.util.*;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i < 11; i++) {
+            System.out.println(n * i);
+
+        }
+
+    }
+}
