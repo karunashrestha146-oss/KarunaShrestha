62ecd5b41dd445e7bf61aa187a072ed02c57d5ed
karunashrestha146-oss <karunashrestha146@gmail.com>
2026-01-14 20:45:01 +0530

    Inverted half pyramid no.s ques

--- InvHalfPy.java
+++ InvHalfPy.java
@@ -1,11 +1,13 @@
 package KarunaShrestha;
 
-public class HalfPyNo {
+//Inverted Half Pyramid
+public class InvHalfPy {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print(j + " ");
+
             }
             System.out.println();
         }
