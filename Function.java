20af3df750dbb4b207da3bcab3c1997380606ebd
karunashrestha146-oss <karunashrestha146@gmail.com>
2026-01-18 23:53:40 +0530

    Function for factorial

--- Function.java
+++ Function.java
@@ -3,15 +3,19 @@ package KarunaShrestha;
 import java.util.*;
 
 public class Function {
-    public static int calculateProduct(int a, int b) {
-        return a * b;
+    public static void PrintFactorial(int n) {
+        int factorial = 1;
+        for (int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+
+        }
+        System.out.println(factorial);
+        return;
     }
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println("product is: " + calculateProduct(a, b));
+        int n = sc.nextInt();
+        PrintFactorial(n);
     }
-
 }
---
+++ Function3.java
@@ -0,0 +1,17 @@
+package KarunaShrestha;
+
+import java.util.*;
+
+public class Function3 {
+    public static int calculateProduct(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("product is: " + calculateProduct(a, b));
+    }
+
+}
