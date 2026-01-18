66ccc6ffa06fdd7f56f0680746642465c1ad06e0
karunashrestha146-oss <karunashrestha146@gmail.com>
2026-01-18 23:28:43 +0530

    Function to add 2 no.s and return the sum

--- Function.java
+++ Function.java
@@ -3,15 +3,17 @@ package KarunaShrestha;
 import java.util.*;
 
 public class Function {
-    public static void printMyName(String name) {
-        System.out.println(name);
-        return;
+    public static int calculateSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+
     }
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        printMyName(name);
-
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = calculateSum(a, b);
+        System.out.println("The sum is: " + sum);
     }
 }
